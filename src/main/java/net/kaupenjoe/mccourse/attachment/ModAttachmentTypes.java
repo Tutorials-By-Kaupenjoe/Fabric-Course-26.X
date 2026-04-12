bb94a2e14b3f12e095f2b997d2280fbc0630d6c9
Kaupenjoe <contact@kaupenjoe.net>
2026-04-12 11:42:01 +0200

    auto-syncing attachment data

--- src/main/java/net/kaupenjoe/mccourse/attachment/ModAttachmentTypes.java
+++ src/main/java/net/kaupenjoe/mccourse/attachment/ModAttachmentTypes.java
@@ -2,13 +2,16 @@ package net.kaupenjoe.mccourse.attachment;
 
 import com.mojang.serialization.Codec;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry;
+import net.fabricmc.fabric.api.attachment.v1.AttachmentSyncPredicate;
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
 import net.kaupenjoe.mccourse.MCCourse;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.Identifier;
 
 public class ModAttachmentTypes {
-    public static final AttachmentType<Integer> MANA = AttachmentRegistry.createPersistent(Identifier.fromNamespaceAndPath(MCCourse.MOD_ID, "mana"),
-            Codec.INT);
+    public static final AttachmentType<Integer> MANA = AttachmentRegistry.create(Identifier.fromNamespaceAndPath(MCCourse.MOD_ID, "mana"),
+            builder -> builder.persistent(Codec.INT).syncWith(ByteBufCodecs.INT, AttachmentSyncPredicate.all()));
 
     public static void registerModAttachments() {
         MCCourse.LOGGER.info("Registering Mod Attachments for " + MCCourse.MOD_ID);
