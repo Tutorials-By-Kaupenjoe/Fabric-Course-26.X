8617711321624090b3fd3a0b488a89fb12d97a9c
Kaupenjoe <contact@kaupenjoe.net>
2026-04-20 18:34:58 +0200

    SpawnPlacements

--- src/main/java/net/kaupenjoe/mccourse/world/gen/ModEntitySpawns.java
+++ src/main/java/net/kaupenjoe/mccourse/world/gen/ModEntitySpawns.java
@@ -3,12 +3,17 @@ package net.kaupenjoe.mccourse.world.gen;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.kaupenjoe.mccourse.entity.ModEntities;
-import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.levelgen.Heightmap;
 
 public class ModEntitySpawns {
     public static void addSpawns() {
         BiomeModifications.addSpawn(BiomeSelectors.includeByKey(Biomes.SAVANNA, Biomes.SAVANNA_PLATEAU, Biomes.BADLANDS),
                 MobCategory.CREATURE, ModEntities.GIRAFFE, 25, 2, 4);
+
+        SpawnPlacements.register(ModEntities.GIRAFFE, SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                PathfinderMob::checkMobSpawnRules);
     }
 }
