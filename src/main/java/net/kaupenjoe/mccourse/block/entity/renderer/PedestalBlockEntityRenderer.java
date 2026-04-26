1edd81edf1f4921c399c815318f8469334ef79fe
Kaupenjoe <contact@kaupenjoe.net>
2026-04-26 20:14:28 +0200

    ghost rendering

--- src/main/java/net/kaupenjoe/mccourse/block/entity/renderer/PedestalBlockEntityRenderer.java
+++ src/main/java/net/kaupenjoe/mccourse/block/entity/renderer/PedestalBlockEntityRenderer.java
@@ -4,6 +4,9 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import net.kaupenjoe.mccourse.block.entity.custom.PedestalBlockEntity;
 import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.block.BlockModelRenderState;
+import net.minecraft.client.renderer.block.BlockModelResolver;
+import net.minecraft.client.renderer.block.model.BlockDisplayContext;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
@@ -12,13 +15,29 @@ import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.phys.Vec3;
+import org.joml.Vector2i;
 import org.jspecify.annotations.Nullable;
+
+import java.util.List;
 
 public class PedestalBlockEntityRenderer implements BlockEntityRenderer<PedestalBlockEntity, PedestalBlockEntityRenderState> {
     private final ItemModelResolver itemModelResolver;
+    private final BlockModelResolver blockResolver;
+    private final BlockModelRenderState blockRenderState = new BlockModelRenderState();
+    List<Vector2i> offsets = List.of(
+            new Vector2i(3, 0),
+            new Vector2i(2, 2),
+            new Vector2i(0, 3),
+            new Vector2i(2, -2),
+
+            new Vector2i(-2, 2),
+            new Vector2i(-2, -2),
+            new Vector2i(0, -3),
+            new Vector2i(-3, 0));
 
     public PedestalBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
         itemModelResolver = context.itemModelResolver();
+        blockResolver = context.blockModelResolver();
     }
 
     @Override
@@ -35,6 +54,7 @@ public class PedestalBlockEntityRenderer implements BlockEntityRenderer<Pedestal
 
         itemModelResolver.updateForTopItem(state.itemStackRenderState,
                 blockEntity.getTheItem(), ItemDisplayContext.FIXED, blockEntity.getLevel(), null, 0);
+        blockResolver.update(blockRenderState, blockEntity.getBlockState(), BlockDisplayContext.create());
     }
 
     @Override
@@ -48,6 +68,19 @@ public class PedestalBlockEntityRenderer implements BlockEntityRenderer<Pedestal
 
         state.itemStackRenderState.submit(poseStack, submitNodeCollector, state.lightCoords, OverlayTexture.NO_OVERLAY, 0);
 
+        poseStack.popPose();
+
+        offsets.forEach(offset -> {
+            if(state.level.getBlockState(state.blockPos.offset(offset.x, 0, offset.y)).isAir()) {
+                renderSidePedestal(poseStack, submitNodeCollector, offset.x, offset.y);
+            }
+        });
+    }
+
+    private void renderSidePedestal(PoseStack poseStack, SubmitNodeCollector submitNodeCollector, float xOffset, float zOffset) {
+        poseStack.pushPose();
+        poseStack.translate(xOffset, 0f, zOffset);
+        blockRenderState.submit(poseStack, submitNodeCollector, 400, OverlayTexture.RED_OVERLAY_V, 0);
         poseStack.popPose();
     }
 }
